f6b20e448a0b8f8cf70bd4fe102fba977b5f4eb2
Jennifer Warren <jennifermaywarren@googlemail.com>
2020-07-21 18:39:39 +0100

    add js

---
+++ port.js
@@ -0,0 +1,23 @@
+
+$(function() {
+
+    $(window).on('wheel', function(e) {
+    
+        var delta = e.originalEvent.deltaY;
+    
+        if (delta > 0) $(".skill-list").css({"bottom": 35+"vw", transition: "1s ease-out"}); //skills move up when you scroll down
+        else $(".skill-list").css({"bottom": (18+"vw"), transition: "1s ease-out"}); //skills come back down when you scroll up
+    });
+    });
+
+
+// $(function() {
+
+//     $(window).on('wheel', function(e) {
+    
+//         var delta = e.originalEvent.deltaY;
+    
+//         if (delta > 0) console.log("meow");
+//         // else $('body').text('up');
+//     });
+//     });
